3c0e8bb7f5a85699a3b1120cbbe958ccfbdeedaa
John <jcsantil@cougarnet.uh.edu>
2021-01-25 01:41:42 -0600

    A variable like user_num can store a value like an integer. Extend the given program as indicated.
    Get a second user input into user_num1.
    Output the user's input. (2 pts)
    Output the input squared and cubed. Hint: Compute squared as user_num * user_num. (2 pts)
    Get a second user input into user_num2, and output the sum and product. (1 pt)

---
+++ Homework-1/main2.py
@@ -0,0 +1,11 @@
+userNum = int(input("Enter integer: \n" ))
+userNumSquared = userNum * userNum
+print("You entered:" , userNum)
+print(userNum , "squared is" , userNumSquared)
+userNumCubed = (userNum * userNum * userNum)
+print("And", userNum , "cubed is" , userNumCubed,"!!")
+userNum2 = int(input("Enter another integer:"))
+userNumAdd = userNum + userNum2
+userNumTimes = userNum * userNum2
+print(userNum, "+", userNum2, "is", userNumAdd)
+print(userNum, "*" , userNum2 , "is" ,userNumTimes)
