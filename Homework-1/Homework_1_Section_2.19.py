6dc337bc3461e1bbda32afc4603fd269a11467f5
John <jcsantil@cougarnet.uh.edu>
2021-02-01 17:06:26 -0600

    CIS 2348 LAB*: Program: Cooking measurement converter

--- Homework-1/Homework_1_Section_2.19.py
+++ Homework-1/Homework_1_Section_2.19.py
@@ -6,20 +6,21 @@ print("\n")
 print("Lemonade ingredients - yields", '{:.2f}'.format(Sc),"servings")
 print('{:.2f} cup(s) lemon juice'.format(Lj))
 print('{:.2f} cup(s) water'.format(Wc))
-print('{:.2f} cup(s) agave nectar'.format(Nc),"\n")
+print('{:.2f} cup(s) agave nectar\n'.format(Nc))
 
 Serv=float(input("How many servings would you like to make?"))
 print("\n")
 print("Lemonade ingredients - yields", '{:.2f}'.format(Serv),"servings")
 print('{:.2f} cup(s) lemon juice'.format(Lj*Serv/Sc))
 print('{:.2f} cup(s) water'.format(Wc*Serv/Sc))
-print('{:.2f} cup(s) agave nectar'.format(Nc*Serv/Sc))
+print('{:.2f} cup(s) agave nectar\n'.format(Nc*Serv/Sc))
+
 
 VolLj=Lj*Serv/Sc
 VolWc=Wc*Serv/Sc
 VolNc=Nc*Serv/Sc
 
-print("Lemonade ingredients - yields",'{:.2f}'.format(Serv),"servings\n")
+print("Lemonade ingredients - yields",'{:.2f}'.format(Serv),"servings")
 print('{:.2f} gallon(s) lemon juice'.format((VolLj)/16))
 print('{:.2f} gallon(s) water'.format((VolWc)/16))
 print('{:.2f} gallon(s) agave nectar'.format((VolNc)/16))
