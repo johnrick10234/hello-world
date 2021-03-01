8ef34eecb099ef58083a8bb56c4fc0d8cf573443
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-01 15:58:45 -0600

    6.22 CIS 2348

--- Homework-2/Homweork_2_6.22.py
+++ Homework-2/Homweork_2_6.22.py
@@ -1,15 +1,21 @@
+# variable 1
 a= int(input())
 b= int(input())
 c= int(input())
-# variables
+# variables 2
 d= int(input())
 e= int(input())
 f= int(input())
 
-for x in range(-10,11):
-    for y in range (-10,11):
-        if a * x + b * y == c and d * x + e * y == f:
+solution=False
+for x in range(-10,10):
+    for y in range (-10,10):
+        c = a * x + b * y
+        f = d * x + e * y
+        # if a * x + b * y == c and d * x + e * y == f:
+        if c == f:
             print (x,y)
-        else:
-            print ('No solution')
-# this solution is not right
+            solution=True
+if not solution:
+    print ("No Solution")
+
