bfd7460483a3a1e9fa85e119cf940130dfd01468
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-01 17:05:54 -0600

    6.22 CIS 2348

--- Homework-2/Homweork_2_6.22.py
+++ Homework-2/Homweork_2_6.22.py
@@ -10,12 +10,11 @@ f= int(input())
 solution=False
 for x in range(-10,10):
     for y in range (-10,10):
-        c = a * x + b * y
-        f = d * x + e * y
-        # if a * x + b * y == c and d * x + e * y == f:
-        if c == f:
+
+        if a * x + b * y == c and d * x + e * y == f:
+
             print (x,y)
             solution=True
 if not solution:
-    print ("No Solution")
+    print ("No solution")
 
