f62e3219b520500c3701dbe6de9d3c295c59edb6
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-03 10:40:42 -0600

    Homework 2 part B update

--- Homework-2/Homework_2_Coding_Problem_2.py
+++ Homework-2/Homework_2_Coding_Problem_2.py
@@ -1,27 +1,31 @@
-# John Rick Santillan ID# 1910045 Part A
+# John Rick Santillan ID# 1910045 Part B
 
 from datetime import datetime
 from datetime import date
 x= date.today()
 months = {"January":"1", 'February':'2','March':'3','April':'4','May':'5','June':'6', 'July':'7', 'August':'8','September':'9','October':'10','November':'11','December':'12'}
+inputs = open("inputDates.txt","r")
+input_line = inputs.read().splitlines()
+for i in input_line:
 
-user_input = input()
-if user_input.find(' ') == -1:
-    exit()
-var1 = user_input.split(' ')
+    user_input = i
+    if user_input.find(' ') ==-1:
+        continue
+    var1 = user_input.split(' ')
 
-var2 = var1[0]
+    var2 = var1[0]
 
+    # var3 = var1[1].split(',')
+    var4 = var1[1][:-1]
+    var5 = var1[2]
 
-var4 = var1[1][:-1]
-var5 = var1[2]
+    if var2 in months:
+        var6 = months.get(var2)+'/'+var4+'/'+var5
 
-if var2 in months:
-    var6 = months.get(var2)+'/'+var4+'/'+var5
+        var7 =datetime.strptime(var6,"%m/%d/%Y").date()
+        if var7 < x:
+            print(var6)
 
-    var7 =datetime.strptime(var6,"%m/%d/%Y").date()
-    if var7 < x:
-        print(var6)
 
 
 
