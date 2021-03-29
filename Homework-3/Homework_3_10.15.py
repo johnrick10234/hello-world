70157eed2b6bc7ea08c5aab135ef9b76c71aa680
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-28 20:59:13 -0500

    Homework 10.15

--- Homework-3/Homework_3_10.15.py
+++ Homework-3/Homework_3_10.15.py
@@ -9,10 +9,15 @@ class Team:
 
 if __name__=="__main__":
     team = Team()
+    team_name=input()
     team_wins = int(input())
     team_losses = int(input())
 
     team.team_wins = team_wins
     team.team_losses=team_losses
 
-    print(team.get_win_percentage())
+
+    if team.get_win_percentage()>=0.5:
+        print('Congratulations','Team',team_name,"has a winning average!")
+    else:
+        print('Team',team_name,'has a losing average.')
