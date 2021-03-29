4eab69434f3ddd918b91c1c6fae312d9d4f5f8c0
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-28 20:08:02 -0500

    Homework 10.11

--- Homework-3/Homework_3_10.11.py
+++ Homework-3/Homework_3_10.11.py
@@ -1,22 +1,22 @@
 # John Rick Santillan     #1910045
 class FoodItem:
 
-    def __init__(self,name='None',fats=0.0,carbs=0.0,proteins=0.0):
+    def __init__(self,name='None',fat=0.0,carbs=0.0,protein=0.0):
         self.name = name
-        self.fats = fats
+        self.fat = fat
         self.carbs = carbs
-        self.proteins = proteins
+        self.protein = protein
 
     def get_calories(self, servings):
         # Calorie formula
-        calories = ((self.fats * 9) + (self.carbs * 4) + (self.proteins * 4)) * servings;
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings;
         return calories
 
     def print_info(self):
         print('Nutritional information per serving of {}:'.format(self.name))
-        print('   Fat: {:.2f} g'.format(self.fats))
+        print('   Fat: {:.2f} g'.format(self.fat))
         print('   Carbohydrates: {:.2f} g'.format(self.carbs))
-        print('   Protein: {:.2f} g'.format(self.proteins))
+        print('   Protein: {:.2f} g'.format(self.protein))
 
 if __name__ == "__main__":
     food1= FoodItem()
@@ -25,10 +25,10 @@ if __name__ == "__main__":
     number_carbs = float(input())
     number_proteins = float(input())
 
-serving = float(input())
+num_servings = float(input())
 food1.print_info()
-print("Number of calories for","{:.2f}".format(serving),"serving(s):","{:.2f}".format(food1.get_calories(serving)))
+print("Number of calories for","{:.2f}".format(num_servings),"serving(s):","{:.2f}".format(food1.get_calories(num_servings)))
 food2 = FoodItem(name_item,number_fats,number_carbs,number_proteins)
 print()
 food2.print_info()
-print("Number of calories for","{:.2f}".format(serving),"serving(s):","{:.2f}".format(food2.get_calories(serving)))
+print("Number of calories for","{:.2f}".format(num_servings),"serving(s):","{:.2f}".format(food2.get_calories(num_servings)))
