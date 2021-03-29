8fab1d8b3a67f8ddbc48c7126e3430b5769bdf8a
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-28 23:06:36 -0500

    Homework 10.17

--- Homework-3/Homework_3_10.17.py
+++ Homework-3/Homework_3_10.17.py
@@ -3,8 +3,8 @@ class ItemToPurchase:
         self.item_name = "none"
         self.item_price=0
         self.item_quantity=0
-    def item_cost(self):
-        print(self.item_name,self.item_quantity,"@","$"+str(self.item_price),'=','$'+str(self.item_price * self.item_quantity))
+    def print_item_cost(self):
+        print(self.item_name,self.item_quantity,"@","$"+str(self.item_price).split('.')[0],'=','$'+str(self.item_price * self.item_quantity).split('.')[0])
 
 if __name__ == "__main__":
     # Item1 = ItemToPurchase()
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     print("Item 1")
     Item1=ItemToPurchase()
     item_name=input("Enter the item name:\n")
-    item_price=float(input("Enter the item price\n"))
-    item_quantity=int(input("Enter the item quantity\n"))
+    item_price=float(input("Enter the item price:\n"))
+    item_quantity=int(input("Enter the item quantity:\n"))
     Item1.item_name = item_name
     Item1.item_price = item_price
     Item1.item_quantity = item_quantity
-    # print()
+    print()
     print("Item 2")
     Item2=ItemToPurchase()
     item_name=input("Enter the item name:\n")
@@ -28,14 +28,13 @@ if __name__ == "__main__":
     Item2.item_quantity = item_quantity
 
 
-    # Item1.item_cost()
-    # Item2.item_cost()
+print()
 total = (Item1.item_price * Item1.item_quantity) + (Item2.item_price * Item2.item_quantity)
 print('TOTAL COST')
-Item1.item_cost()
-Item2.item_cost()
+Item1.print_item_cost()
+Item2.print_item_cost()
 print()
-print('Total:','$'+str(total))
+print('Total:','$'+str(total).split('.')[0])
 
 
 
