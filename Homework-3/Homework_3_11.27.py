550680eb482be1adea719a9d1dea01b788e8e1dc
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-03-31 21:33:03 -0500

    Homework 11.27

---
+++ Homework-3/Homework_3_11.27.py
@@ -0,0 +1,16 @@
+p_dict = {}
+sort_list = []
+
+def sort_key():
+    s_key = sorted(p_dict.keys())
+    return s_key
+
+while(True):
+    print('MENU')
+    print('a - Add player')
+    print('d - Remove player')
+    print('u - Update player rating')
+    print('r - Output players above a rating')
+    print('o - Output roster')
+    print('q - Quit\n')
+    print(input("Choose an option: "))
