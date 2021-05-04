a7c26a0286350666928d69afdde4c57355138f6b
johnrick10234 <jcsantil@cougarnet.uh.edu>
2021-05-03 20:11:16 -0500

    Homework

--- Homework-4/Homework_4_14.16.py
+++ Homework-4/Homework_4_14.16.py
@@ -1,31 +1,30 @@
-# Global variable
+## Global variable
 num_calls = 0
-
 
 # TODO: Write the partitioning algorithm - pick the middle element as the
 #       pivot, compare the values using two index variables l and h (low and high),
 #       initialized to the left and right sides of the current elements being sorted,
 #       and determine if a swap is necessary
 def partition(user_ids, i, k):
-    pivot = user_ids[k]
-    index = i - 1
-    for j in range(i, k):
-        if user_ids[j] <= pivot:
-            index += 1
-            user_ids[index], user_ids[j] = user_ids[j], user_ids[index]
-            user_ids[index + 1], user_ids[k] = user_ids[k], user_ids[index + 1]
-    return index + 1
-
+   pivot = user_ids[k]
+   index = i -1
+   for j in range(i, k):
+       if user_ids[j] <= pivot:
+           index += 1
+           user_ids[index], user_ids[j] = user_ids[j], user_ids[index]
+   user_ids [index+1], user_ids[k] = user_ids[k], user_ids[index+1]
+   return index+1
 
 def quicksort(user_ids, i, k):
     global num_calls
-    num_calls += 1
-    if i < k:
-        num_calls += 1
-        piv = partition(user_ids, i, k)
-        quicksort(user_ids, i, piv - 1)
-        quicksort(user_ids, piv + 1, k)
+    num_calls = int(2 * len(user_ids)-1)
 
+    # num_calls +=1
+    if i < k:
+        # num_calls +=1
+        piv = partition(user_ids, i,k)
+        quicksort(user_ids, i, piv-1)
+        quicksort(user_ids, piv+1,k)
 
 if __name__ == "__main__":
     user_ids = []
